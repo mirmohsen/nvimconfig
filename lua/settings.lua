00f7a05f49c4b25299d5aebde33dac59c5650845
mohsen <m.mirazimi@coinlocally.com>
2025-12-14 17:26:07 +0330

    Add initial configuration files for Neovim settings

---
+++ lua/settings.lua
@@ -0,0 +1,21 @@
+--? Leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--? Editor behavior
+vim.opt.backspace = {"indent", "eol", "start"}
+vim.opt.showcmd = true
+vim.opt.laststatus = 3
+vim.opt.autowrite = true
+vim.opt.autoread = true
+vim.opt.cursorline = true
+vim.opt.number = true
+
+--? Tabs & indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+--? Keymap
+vim.keymap.set('n', '<leader>h', vim.cmd.nohlsearch)
